93e7de62cf4f75d1065fb251bca29d6fb8b513f5
claudio Fernandes <claudio.fe.rejes@hotmail.com>
2024-11-25 21:22:25 -0300

    Feat: Iniciando modulo VPC - projeto final ECS

---
+++ vpc/variables.tf
@@ -0,0 +1,37 @@
+variable "project_name" {
+  description = "Nome do Projeto, essa variável será utilizada para nomear os recursos criados como prefixo."
+
+}
+
+variable "region" {
+  description = "Região AWS onde os recursos serão criados."
+
+}
+
+variable "cidr" {
+  description = "CIDR block da VPC."
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "Zonas de disponibilidade onde as subnets serão criadas."
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "SUbnetes privadas para comunicação entre os serviços."
+  type = list(map({
+    name               = string
+    cidr               = string
+    availability_zones = string
+  }))
+}
+
+variable "public_subnets" {
+  description = "SUbnetes publicas para comunicação entre os serviços."
+  type = list(map({
+    name               = string
+    cidr               = string
+    availability_zones = string
+  }))
+}
